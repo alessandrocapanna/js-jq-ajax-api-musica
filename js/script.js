39f01dff4c796af90bd2c6e0f3da258f992b42ea
alessandrocapanna <acapanna75@gmail.com>
2020-06-26 19:17:47 +0200

    con bonus

--- js/script.js
+++ js/script.js
@@ -1,43 +1,52 @@
-// Attraverso una chiamata ajax all’Api di boolean avremo a
-// disposizione una decina di dischi musicali.
-// Servendoci di handlebars stampiamo tutto a schermo.
-// In questo momento non è importante la parte grafica.
 $(document).ready(
   function(){
+    // AJAX
     $.ajax(
       {
-        // endpoint API  method
+        // endpoint API e method
         url: 'https://flynn.boolean.careers/exercises/api/array/music',
         method: 'GET',
 
         // in caso di successo
         success: function(canzoneRandom) {
-          // Inserisco canzone random nella variabile
+          // Inserisco canzone random nella variabile canzone
           var canzone = canzoneRandom.response;
 
           // Struttura dinamica con handlebars
           var source = $('#entry-template').html();
           var template = Handlebars.compile(source);
 
+          //ciclo per appendere tutte le canzoni nell'html
           for (var i = 0; i < canzone.length; i++) {
             var context = canzone[i];
-            console.log(context);
+
             var html = template(context);
             $('.cds-container').append(html);
           }
 
-          // $(document).on('change','.select',
-          //   function(){
-          //
-          //     var
-          //     if ($('select').val() == $(this)) {
-          //       console.log('lol');
-          //     }
-          //   }
-          // );
+          // al change della select
+          $('.select').on('change',
+            function(){
+              // creo bariabile del genere selezionato
+              var genereMusicale = $(this).val().toLowerCase();
 
-
-
+              // se seleziono tutti sono tutti visibili
+              if (genereMusicale == 'tutti') {
+                $('.cd').removeClass('hidden');
+              }else {
+                // altrimenti faccio un each per vedere tutti i .cd
+                $('.cd').each(
+                  function(){
+                    if ($(this).find('.genre').text().toLowerCase() == genereMusicale) { // levo classe hidden al .genre == a genereMusicale 
+                    $(this).removeClass('hidden');
+                  } else { // aggiungo .hidden a tutti gli altri
+                    $(this).addClass('hidden');
+                  }
+                  }
+                );
+              }
+            }
+          );
 
         },
         error:
